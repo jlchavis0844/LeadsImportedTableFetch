b5bba6c592bada5faff008555af0a2e2ae1215c8
jchavis <jchavis@DBA>
2018-03-07 10:31:25 -0800

    Add project files.

---
+++ LeadsImportedTableFetch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeadsImportedTableFetch {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
